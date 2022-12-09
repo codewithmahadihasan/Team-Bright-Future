a55c7e6c00432365e728cbe83ba2a058cf594d07
loneliness-mahadi <lonelinessmahadi@gmail.com>
2022-12-09 15:02:13 +0600

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import NavberNo1 from './Components/Navber/NavberNo1/NavberNo1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <NavberNo1></NavberNo1>
     </div>
   );
 }
